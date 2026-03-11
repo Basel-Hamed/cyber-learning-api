5bbb0b15a9484f6ecce6646543a5cd7ab9407728
Basel-Hamed <scef.baselhamed.bd@gmail.com>
2026-03-11 23:47:58 +0600

    Add Bengali translation function using googletrans

---
+++ translator.py
@@ -0,0 +1,19 @@
+from googletrans import Translator
+
+translator=Translator()
+
+def translate_bn(text,style):
+
+    try:
+
+        result=translator.translate(text,dest="bn").text
+
+        if style=="sadhu":
+
+            result=result.replace("করে","করিয়া").replace("হয়","হইয়াছে")
+
+        return result
+
+    except:
+
+        return "বাংলা অনুবাদ করা যায়নি"
