4d66e91c8eba794deca2ba77b0c0124f058ebc69
Basel-Hamed <scef.baselhamed.bd@gmail.com>
2026-03-11 23:15:32 +0600

    Add a collection of security training sites

---
+++ sites.py
@@ -0,0 +1,192 @@
+SITES = {
+
+    "owasp": {
+        "name": "OWASP",
+        "url": "https://owasp.org",
+        "category": "Web Security"
+    },
+
+    "portswigger": {
+        "name": "PortSwigger Web Security Academy",
+        "url": "https://portswigger.net/web-security",
+        "category": "Web Hacking"
+    },
+
+    "hackthebox": {
+        "name": "Hack The Box Academy",
+        "url": "https://academy.hackthebox.com",
+        "category": "Ethical Hacking"
+    },
+
+    "tryhackme": {
+        "name": "TryHackMe",
+        "url": "https://tryhackme.com",
+        "category": "Cyber Security Training"
+    },
+
+    "cybrary": {
+        "name": "Cybrary",
+        "url": "https://www.cybrary.it",
+        "category": "Cyber Security Courses"
+    },
+
+    "sans": {
+        "name": "SANS Institute",
+        "url": "https://www.sans.org",
+        "category": "Security Training"
+    },
+
+    "kali": {
+        "name": "Kali Linux Training",
+        "url": "https://www.kali.org/docs/",
+        "category": "Penetration Testing"
+    },
+
+    "pentesterlab": {
+        "name": "PentesterLab",
+        "url": "https://pentesterlab.com",
+        "category": "Web Security"
+    },
+
+    "hackerone": {
+        "name": "HackerOne Hacker Resources",
+        "url": "https://www.hackerone.com/resources",
+        "category": "Bug Bounty"
+    },
+
+    "bugcrowd": {
+        "name": "Bugcrowd University",
+        "url": "https://www.bugcrowd.com/hackers/bugcrowd-university/",
+        "category": "Bug Bounty"
+    },
+
+    "securitytube": {
+        "name": "SecurityTube",
+        "url": "http://www.securitytube.net",
+        "category": "Hacking Tutorials"
+    },
+
+    "nullbyte": {
+        "name": "Null Byte",
+        "url": "https://null-byte.wonderhowto.com",
+        "category": "Hacking Tutorials"
+    },
+
+    "stationx": {
+        "name": "StationX",
+        "url": "https://www.stationx.net",
+        "category": "Cyber Security Training"
+    },
+
+    "opensecuritytraining": {
+        "name": "Open Security Training",
+        "url": "http://opensecuritytraining.info",
+        "category": "Security Courses"
+    },
+
+    "hacksplaining": {
+        "name": "Hacksplaining",
+        "url": "https://www.hacksplaining.com",
+        "category": "Web Security"
+    },
+
+    "codecademy_security": {
+        "name": "Codecademy Cyber Security",
+        "url": "https://www.codecademy.com/learn/introduction-to-cybersecurity",
+        "category": "Beginner Cyber Security"
+    },
+
+    "edx_security": {
+        "name": "edX Cyber Security",
+        "url": "https://www.edx.org/learn/cybersecurity",
+        "category": "University Courses"
+    },
+
+    "coursera_security": {
+        "name": "Coursera Cyber Security",
+        "url": "https://www.coursera.org/browse/information-technology/cybersecurity",
+        "category": "Cyber Security Courses"
+    },
+
+    "mit_security": {
+        "name": "MIT OpenCourseWare Security",
+        "url": "https://ocw.mit.edu",
+        "category": "University Security"
+    },
+
+    "stanford_security": {
+        "name": "Stanford Security Lab",
+        "url": "https://seclab.stanford.edu",
+        "category": "Security Research"
+    },
+
+    "google_security": {
+        "name": "Google Security Training",
+        "url": "https://security.google",
+        "category": "Security Research"
+    },
+
+    "microsoft_security": {
+        "name": "Microsoft Security Learning",
+        "url": "https://learn.microsoft.com/security",
+        "category": "Security Training"
+    },
+
+    "cisco_security": {
+        "name": "Cisco Cyber Security",
+        "url": "https://www.netacad.com",
+        "category": "Networking Security"
+    },
+
+    "offensive_security": {
+        "name": "Offensive Security",
+        "url": "https://www.offsec.com",
+        "category": "Penetration Testing"
+    },
+
+    "ec_council": {
+        "name": "EC Council",
+        "url": "https://www.eccouncil.org",
+        "category": "Ethical Hacking"
+    },
+
+    "rootme": {
+        "name": "Root Me",
+        "url": "https://www.root-me.org",
+        "category": "Hacking Practice"
+    },
+
+    "vulnhub": {
+        "name": "VulnHub",
+        "url": "https://www.vulnhub.com",
+        "category": "Practice Labs"
+    },
+
+    "ctftime": {
+        "name": "CTFtime",
+        "url": "https://ctftime.org",
+        "category": "Capture The Flag"
+    },
+
+    "overTheWire": {
+        "name": "OverTheWire",
+        "url": "https://overthewire.org",
+        "category": "Security Games"
+    },
+
+    "picoctf": {
+        "name": "PicoCTF",
+        "url": "https://picoctf.org",
+        "category": "CTF Training"
+    },
+
+    # ❌ DELETE SITE LIKE THIS
+    # শুধু নিচের ব্লকটা delete করলেই site remove হবে
+
+    # "example": {
+    #     "name": "Example Site",
+    #     "url": "https://example.com",
+    #     "category": "Test"
+    # }
+
+}
