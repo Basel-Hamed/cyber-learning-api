cea8449d5e96a5724a0aa0ab56a67874ae8b55b6
Basel-Hamed <scef.baselhamed.bd@gmail.com>
2026-03-12 12:22:47 +0600

    Switch to deep-translator for Bangla translation
    Updated translation implementation to use deep-translator library.

--- translator.py
+++ translator.py
@@ -1,21 +1,16 @@
-from googletrans import Translator
-
-translator = Translator()
-
+from deep_translator import GoogleTranslator
 
 def translate_bn(text, style):
-
+    """
+    Translate text to Bangla using deep-translator.
+    style: 'colloquial' or 'sadhu'
+    """
     try:
-
-        result = translator.translate(text, dest="bn").text
-
+        result = GoogleTranslator(source='auto', target='bn').translate(text)
         if style == "sadhu":
-
+            # Optional minor modifications for sadhu style
             result = result.replace("করে", "করিয়া")
             result = result.replace("হয়", "হইয়াছে")
-
         return result
-
     except:
-
         return "বাংলা অনুবাদ করা যায়নি"
